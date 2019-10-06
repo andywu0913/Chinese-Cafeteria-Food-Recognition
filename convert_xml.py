5379a6bd48bf609a14607daeea34bd226181dfd3
Winnie Tsou <winnie22935@gmail.com>
2019-10-06 12:57:18 +0800

    Add function (write to file).
    Former-commit-id: 46720a511be05a8c1fe7dd79849fe11a4a4b4c84 [formerly b5b5e4a0c4c913ceeb5f203f99843e4425b22db4 [formerly 008998d28f3200a01c22356a80d56078a2876e18]]
    Former-commit-id: 0795c83b8adb8b12762cd817f1ae98b225232cf2
    Former-commit-id: 2514781ee034b66d50f7d77d9ce6b5d0f7b85e89 [formerly 08171c7f22f7abe736df20146c445549068c2b04]
    Former-commit-id: 1332f8724073c94466b83f0dabefa9fa27691f62

--- convert_xml.py
+++ convert_xml.py
@@ -16,6 +16,10 @@ from xmljson import badgerfish as bf
 # path/to/img2.jpg 120,300,250,600,2
 
 
+# python convert_xml.py PATH_TO_CLASS_FILE PATH_TO_IMAGE_XML_FILE DEST_OF_FILE
+# ex:  python convert_xml.py ~/Desktop/class.txt ~/Desktop/image ~/Desktop/train.txt
+
+
 def check_image_file_exist(filename):
 	image_file_type = ['.bmp', '.jpg', '.jpeg', '.png', '.gif']
 	filename = os.path.splitext(filename)[0]
@@ -36,8 +40,6 @@ def get_obj_info(obj):
 	return (x_min, y_min, x_max, y_max, class_name)
 
 
-
-# python convert_to_xml.py obj.names directory/
 names_filename = sys.argv[1]
 class_arr = {}
 class_id = 0
@@ -53,14 +55,14 @@ with open(names_filename, 'r') as names_file:
 
 directory = sys.argv[2]
 
-tmp = ''
+content = ''
 
 for filename in os.listdir(directory):
 	filename = os.path.join(directory, filename)
 	if filename.endswith('.xml'):
 		image_filename = check_image_file_exist(filename)
 		if image_filename != None:
-			tmp += image_filename + ' '
+			content += image_filename + ' '
 		else:
 			print('Image file not found.')
 
@@ -73,9 +75,13 @@ for filename in os.listdir(directory):
 
 			for obj in obj_arr:
 				(x_min, y_min, x_max, y_max, class_name) = get_obj_info(obj)
-				tmp += str(x_min) + ',' + str(y_min) + ',' + str(x_max) + ',' + str(y_max) + ',' + str(class_arr[class_name]) + ' '
+				content += str(x_min) + ',' + str(y_min) + ',' + str(x_max) + ',' + str(y_max) + ',' + str(class_arr[class_name]) + ' '
 
-			tmp += '\n'
+			content += '\n'
 
-print(tmp)
+
+train_txt_file = sys.argv[3]
+
+with open(train_txt_file, 'a') as dest_file:
+	dest_file.write(content)
 
