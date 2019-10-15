d1e95c5e5e62066b56b54d07ba55f41ec7da7b52
Andy Wu <andy@brz01.poss>
2019-10-15 14:22:23 +0800

    Add training necessities.
    Former-commit-id: 28d4b493f41a846830a020e88b483b48ba16df5f [formerly 1ee7100450ed06eeb595dad3833c9994920db8ba [formerly 9f1f7d54bb8cc97e3b4c1227eca896c9ee023105]]
    Former-commit-id: 13202adb50b157b1812b0c3c0730d9a13ed4810b
    Former-commit-id: 1a53df3ab83d26a546958b24ac4065ce47194a35 [formerly e4c803aae358df8214351d6a1b26d51a206f2780]
    Former-commit-id: d424318cc93238b5cce0fbce7a043c240904cb68

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from yolo3.utils import get_random_data
 
 
 def _main():
-    annotation_path = 'annotation.txt'
+    annotation_path = 'train.txt'
     log_dir = 'logs/000/'
     classes_path = 'classes.txt'
     anchors_path = 'model_data/yolo_anchors.txt'
@@ -30,7 +30,7 @@ def _main():
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='logs/000/trained_weights_final.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='model_data/yolov3.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -49,8 +49,8 @@ def _main():
 
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
-    if False:
-        model.compile(optimizer=Adam(lr=1e-13), loss={
+    if True:
+        model.compile(optimizer=Adam(lr=1e-3), loss={
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
@@ -60,7 +60,7 @@ def _main():
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=20,
+                epochs=10,
                 initial_epoch=0,
                 callbacks=[logging, checkpoint])
         model.save_weights(log_dir + 'trained_weights_stage_1.h5')
@@ -70,7 +70,7 @@ def _main():
     if True:
         for i in range(len(model.layers)):
             model.layers[i].trainable = True
-        model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        model.compile(optimizer=Adam(lr=1e-3), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
         batch_size = 4 # note that more GPU memory is required after unfreezing the body
@@ -79,9 +79,9 @@ def _main():
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=100,
+            epochs=5,
             initial_epoch=0,
-            callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+            callbacks=[logging, checkpoint, reduce_lr, early_stopping]) #, early_stopping
         model.save_weights(log_dir + 'trained_weights_final.h5')
 
     # Further training if needed.
